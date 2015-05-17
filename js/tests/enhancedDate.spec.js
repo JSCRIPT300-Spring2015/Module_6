452f8716efa7bb512d6ae95de2c24f7fae5c8278
bernie macias <berniem@zillow.com>
2015-05-17 10:10:59 -0700

    changed tests to show setDate

--- js/tests/enhancedDate.spec.js
+++ js/tests/enhancedDate.spec.js
@@ -1,73 +1,80 @@
 describe("enhancedDate", function ()  {
   'use strict';
-  var epoch = 1421832287101;
-  var _date = new Date(epoch);
-  var _cDate = new Date();
-  var _future;
+
   describe("setDate", function () {
 
     it("Should instance of date", function () {
-      expect(enhancedDate.setDate()).to.be.a("Date");
+      expect(enhancedDate.setDate(new Date(2015, 4, 23))).to.be.a("Date");
     });
 
-    it("Should return epoch time: " + epoch, function () {
-      expect(enhancedDate.setDate(_date).getTime()).to.be.equal(epoch);
+    it("Should return epoch time: " + (new Date(2015, 4, 23)), function () {
+      expect(enhancedDate.setDate(new Date(2015, 4, 23))).to.be.deep.equal(new Date(2015, 4, 23));
     });
   });
 
   describe("getDate",function () {
 
     it("Should be typeof number", function () {
+      enhancedDate.setDate(new Date(2015, 4, 23));
       expect(enhancedDate.getDate()).to.be.a("number");
     });
 
-    it("Should be equal to epoch time: " + epoch, function () {
-      expect(enhancedDate.getDate()).to.be.equal(epoch);
+    it("Should be equal to epoch time: " + (new Date(2015, 4, 23)), function () {
+      enhancedDate.setDate(new Date(2015, 4, 23));
+      expect(enhancedDate.getDate()).to.be.equal((new Date(2015, 4, 23)).getTime());
     });
   });
 
   describe("getDayName",function () {
 
     it("Should be typeof string", function () {
+      enhancedDate.setDate(new Date(2015, 4, 23));
       expect(enhancedDate.getDayName()).to.be.a("string");
     });
 
     it("Should be equal to Wednesday", function () {
-      expect(enhancedDate.getDayName()).to.be.equal("Wednesday");
+      enhancedDate.setDate(new Date(2015, 4, 23));
+      expect(enhancedDate.getDayName()).to.be.equal("Saturday");
     });
 
     it("Should not be equal to Saturday", function () {
-      expect(enhancedDate.getDayName()).to.not.be.equal("Saturday");
+      enhancedDate.setDate(new Date(2015, 4, 23));
+      expect(enhancedDate.getDayName()).to.not.be.equal("Monday");
     });
   });
 
   describe("getMonthName",function () {
 
     it("Should be typeof string", function () {
+      enhancedDate.setDate(new Date(2015, 4, 23));
       expect(enhancedDate.getMonthName()).to.be.a("string");
     });
 
-    it("Should be equal to January", function () {
-      expect(enhancedDate.getMonthName()).to.be.equal("January");
+    it("Should be equal to April", function () {
+      enhancedDate.setDate(new Date(2015, 4, 23));
+      expect(enhancedDate.getMonthName()).to.be.equal("May");
     });
 
     it("Should not be equal to May", function () {
-      expect(enhancedDate.getMonthName()).to.not.be.equal("May");
+      enhancedDate.setDate(new Date(2015, 4, 23));
+      expect(enhancedDate.getMonthName()).to.not.be.equal("April");
     });
   });
 
   describe("isToday",function () {
 
     it("Should be typeof boolean", function () {
+      enhancedDate.setDate(new Date(2015, 4, 23));
       expect(enhancedDate.isToday()).to.be.a("boolean");
     });
 
-    it("Should be " + epoch + " false", function () {
+    it("Should be " + (new Date(2015, 4, 23)) + " false", function () {
+      enhancedDate.setDate(new Date(2015, 4, 23));
       expect(enhancedDate.isToday()).to.be.equal(false);
     });
 
-    it("Should be " + _cDate.getTime() + " true", function () {
-      enhancedDate.setDate(_cDate);
+    it("Should be " + (new Date()) + " true", function () {
+      enhancedDate.setDate(new Date());
       expect(enhancedDate.isToday()).to.be.equal(true);
     });
   });
@@ -78,13 +85,13 @@ describe("enhancedDate", function ()  {
       expect(enhancedDate.isFuture()).to.be.a("boolean");
     });
 
-    it("Should be " + _cDate.getTime() + " false", function () {
+    it("Should be " + (new Date()).getTime() + " false", function () {
+      enhancedDate.setDate(new Date());
       expect(enhancedDate.isFuture()).to.be.equal(false);
     });
 
-    _future = _cDate.getTime() + 100;
-    it("Should be " + _future + " true", function () {
-      enhancedDate.setDate(_future);
+    it("Should be " + ((new Date()).getTime() + 500) + " true", function () {
+      enhancedDate.setDate((new Date()).getTime() + 500);
       expect(enhancedDate.isFuture()).to.be.equal(true);
     });
   });
