00f98dbb0d41f0d48123e55527b21e71418618fd
Carolyn Velez <carolyn.g.velez@gmail.com>
2015-05-26 22:41:29 -0700

    final commit
    Here are my final changes after watching your video. It’s good to know
    I was making it way too complicated. I think I have a better feel for
    what to do now. I have one question about testing isFuture. If the date
    is today, it’s also not in the future. Do we not test for that in
    isFuture because we’re already testing for it isToday?

--- js/tests/enhancedDate.spec.js
+++ js/tests/enhancedDate.spec.js
@@ -1,69 +1,100 @@
 // describing spec (what's being tested)
 describe("enhancedDate", function () {
 
-	// Test each method within object
-
 	// setDate
 	describe("setDate", function () {
 
 		it("Return same date object as date object given.", function () {
 			var newDate = new Date("Dec 12, 2015");
-			var newDateString = newDate.toString();
-			var setDateResult = enhancedDate.setDate(newDate);
-			var setDateString = setDateResult.toString();
 
-			expect(newDateString).to.equal(setDateString);
+			expect(enhancedDate.setDate(newDate)).to.deep.equal(new Date(newDate));
 		});
 
 		it("Give unix time then get date object back that represents that same time.", function () {
 			var dateNumber = 1432406605;
-			var setDateResult = enhancedDate.setDate(dateNumber);
-			var setDateNumber = setDateResult.valueOf();
 
-			expect(dateNumber).to.equal(setDateNumber);
+			expect(enhancedDate.setDate(dateNumber)).to.deep.equal(new Date(dateNumber));
+		});
+
+		it("Should return date object.", function () {
+			expect(enhancedDate.setDate()).to.be.an.instanceOf(Date);
+		});
+
+		it("Should return an object identical to the one passed in.", function () {
+			expect(enhancedDate.setDate(new Date(2015, 4, 26))).to.be.deep.equal(new Date(2015, 4, 26));
 		});
 	});
 
 	// getDate
 	describe("getDate", function () {
 
-		it("", function () {
-
+		it("Should return a date object if true is passed in.", function () {
+			expect(enhancedDate.getDate(true)).to.be.an.instanceOf(Date);
 		});
+
+		it("Should return a number if nothinng is passed in.", function () {
+			expect(enhancedDate.getDate()).to.be.a('number');
+		});
+
 	});
 
 	// getDayName
 	describe("getDayName", function () {
 
-		it("Create a new Date object to set date with.", function () {
-			var dayName = enhancedDate(new Date(2015, 4, 14));
-
-			expect(dayName).to.equal("Thursday");
+		before(function () {
+		    enhancedDate.setDate(new Date(2015, 4, 26));
+		});
+		
+		it('Should return a string.', function () {
+		    expect(enhancedDate.getDayName()).to.be.a('string');
+		});
+		
+		it('Should return "Tuesday".', function () {
+		    expect(enhancedDate.getDayName()).to.equal('Tuesday');
 		});
 	});
 
 	// getMonthName
 	describe("getMonthName", function () {
 
-		it("", function () {
+		before(function () {
+		    enhancedDate.setDate(new Date(2015, 4, 26));
+		});
 
+		it('Should return a string.', function () {
+		    expect(enhancedDate.getMonthName()).to.be.a('string');
+		});
+
+		it('Should return "May".', function () {
+		    expect(enhancedDate.getMonthName()).to.equal('May');
 		});
 	});
 
 	// isToday
 	describe("isToday", function () {
+	
+	    it("Should return true if it is today.", function () {
+	        enhancedDate.setDate(new Date());
+		    expect(enhancedDate.isToday()).to.be.true
+	    });
 
-		it("", function () {
-
-		});
+	    it("Should return false if it is not today.", function () {
+	        enhancedDate.setDate(new Date(2014, 4, 26));
+	        expect(enhancedDate.isToday()).to.be.false;
+	    });
 	});
 
 	// isFuture
 	describe("isFuture", function () {
+	
+	    it("Should return true if the date is in the future.", function () {
+	        enhancedDate.setDate(new Date(2120, 4, 26));
+	        expect(enhancedDate.isFuture()).to.be.true;
+	    });
 
-		it("", function () {
-
-		});
+	    it("Should return false if the date is not in the future.", function () {
+	        enhancedDate.setDate(new Date(Date));
+	        expect(enhancedDate.isFuture()).to.be.false;
+	    });
 	});
-
 });
