c43483b5bca5e33de4d8aca0a812acc1f81b05da
Eddie Lee <eddielee@email.com>
2015-06-07 17:29:48 -0700

    first commit

---
+++ js/tests/enhancedDate.spec.js
@@ -0,0 +1,80 @@
+describe('enhancedDate', function(){
+	describe('setDate', function() {
+
+		it('should return a Date object', function() {
+			expect(enhancedDate.setDate()).to.be.an.instanceof(Date);
+		});
+		
+		it('should return an object identical to the one passed in', function() {
+			expect(enhancedDate.setDate(new Date(2015, 6, 7))).to.be.deep.equal(new Date(2015, 6, 7));
+		});
+	});
+
+	describe('getDate', function() {
+		
+		it('should return a Date object if true is passed in', function() {
+			expect(enhancedDate.getDate(true)).to.be.an.instanceof(Date);
+		});
+		
+		it('should return a number if nothing is passed in', function() {
+			expect(enhancedDate.getDate()).to.be.a('number');
+		});
+	});
+
+	describe('getDayName', function() {
+
+		before(function() {
+			enhancedDate.setDate(new Date("6/7/2015"));
+		});
+		
+		it('should return a string', function() {
+			expect(enhancedDate.getDayName()).to.be.a('string');
+		});
+		
+		it('should return "Sunday"', function() {
+			expect(enhancedDate.getDayName()).to.equal('Sunday');
+		});
+	});
+
+	describe('getMonthName', function() {
+
+		before(function() {
+			enhancedDate.setDate(new Date("6/7/2015"));
+		});
+		
+		it('should return a string', function() {
+			expect(enhancedDate.getMonthName()).to.be.a('string');
+		});
+		
+		it('should return "June"', function() {
+			expect(enhancedDate.getMonthName()).to.equal('June');
+		});
+	});	
+
+	describe('isToday', function() {
+		
+		it('should return a true if it is today', function() {
+			enhancedDate.setDate(new Date());
+			expect(enhancedDate.isToday()).to.be.true;
+		});
+		
+		it('should return false if it is not today', function() {
+			enhancedDate.setDate(new Date(2014, 4, 26));
+
+			expect(enhancedDate.isToday()).to.be.false;
+		});
+	});	
+
+	describe('isFuture', function() {
+		
+		it('should return a true if it is in the future', function() {
+			enhancedDate.setDate(new Date("6/7/2116"));
+			expect(enhancedDate.isFuture()).to.be.true;
+		});
+		
+		it('should return false if it is in the past', function() {
+			enhancedDate.setDate(new Date("6/5/2014"));
+			expect(enhancedDate.isFuture()).to.be.false;
+		});
+	});	
+});
